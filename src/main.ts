92a0b0a6cb080240889b8dcfd3e4e96fa80262c1
Jonas Nicoletti <sjnao89@gmail.com>
2020-06-13 11:12:23 +0200

    (deploy) env variables

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.SERVER_PORT || 4000);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
